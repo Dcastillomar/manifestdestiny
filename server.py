325b313adfb31fcf42d63bb101d7dd967f5bb8dd
Dcastillomar <125750537+Dcastillomar@users.noreply.github.com>
2024-03-12 20:46:41 -0500

    edit server.py to work on heroku server

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import random
+import os
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 
@@ -88,4 +89,5 @@ def submit_form():
     else:
             return jsonify({'question': None, 'answers': []})
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
